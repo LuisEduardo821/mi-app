4c002d0f5d5a707cf76c51b4a42669dc8eb74616
LuisEduardo821 <lerivadeneira@pucesi.edu.ec>
2023-09-08 17:35:31 -0500

    primer commit

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Testimonio() {
+  return (
+    <div className="contenedor-testimonio">
+      <div>
+        <img
+          className="imagen-estimonio"
+          src={require("../imagenes/testimonio-Emma.png")}
+          alt="imagen-emma"
+        />
+      </div>
+      <div></div>
+    </div>
+  );
+}
