1e73974ab43762ae86a6edab21fafdfdce4969fa
LuisEduardo821 <lerivadeneira@pucesi.edu.ec>
2023-09-12 17:52:11 -0500

    tercer commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import "./App.css";
-import Testimonio from "./componentes/Testimonio.js";
+import Testimonio from "./componentes/Testimonio";
 
 function App() {
   return (
     <div className="App">
       <div className="contenedor-principal">
         <h1>Here is what our alumni say about freeCodeCamp:</h1>
-        <Testimonio />
+        <Testimonio
+          nombre="Shawn Wang"
+          imagen="Shawn"
+          pais="Singapur"
+          cargo="Ingeniero de software"
+          empresa="Amazon"
+          testimonio="Da miedo cambiar de carrera. Solo gané confianza en que podía codificar trabajando durante cientos de horas de lecciones gratuitas en freeCodeCamp. En un año tenía un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida . "
+        />
+        <Testimonio
+          nombre="Sara Chima"
+          imagen="Sarah"
+          pais="Nigeria"
+          cargo="Ingeniero de software"
+          empresa="ChatDesk"
+          testimonio="freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de codificación desde un nivel principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollo en una empresa increíble."
+        />
+        <Testimonio
+          nombre="Emma Bostian"
+          imagen="emma"
+          pais="Suecia"
+          cargo="Ingeniero de software"
+          empresa="Spotify"
+          testimonio="Siempre me ha costado aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."
+        />
       </div>
     </div>
   );
